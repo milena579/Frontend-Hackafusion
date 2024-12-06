f38624a5e9d3f19c48fce09662d9b9c0c703a160
milena579 <milecalegari@gmail.com>
2024-12-06 09:27:36 -0300

    form projeto praticamente finalizado

--- src/app/novoProjeto/page.tsx
+++ src/app/novoProjeto/page.tsx
@@ -1,21 +1,33 @@
 "use client"
 
 import { Menu } from "@/components/menu"
+import { useState } from "react"
 
 export default function novoProjeto(){
+    
+    const [nomeProjeto, setNomeProjeto] =  useState<string>("")
+    const [tema, setTema] =  useState<string>("")
+    const [publico, setPublico] =  useState<boolean>()
+    const [participantes, setParticipante] =  useState<string>("")
+    const [descricao, setDescricao] =  useState<string>("")
+    
+    const CriarProjeto=  async () =>{
+
+    }
     return(
         <>
+
             <Menu op1="Fóruns" op2="Projetos" op3="Discussões"></Menu>
 
             <div className="flex w-full flex-col items-center px-64 py-20 h-screen justify-start gap-20">
                 <div className="items-center w-[90%] flex justify-center gap-10 ">
                     <div className="flex flex-col w-[40%] gap-4">
                         <label htmlFor="nome">Nome</label>
-                        <input type="text" name="nome" className="border" />
+                        <input type="text" name="nome" className="border" value={nomeProjeto} onChange={(event) => {setNomeProjeto(event.target.value)}} />
                     </div>
                     <div className="flex flex-col w-[40%] gap-4">
                         <label htmlFor="tema">Tema</label>
-                        <input type="text" name="tema" className="border" />
+                        <input type="text" name="tema" className="border" value={tema} onChange={(event) => {setTema(event.target.value)}} />
                     </div>
                 </div>
                 <div className="w-[90%] flex justify-center gap-10 ">
@@ -24,17 +36,17 @@ export default function novoProjeto(){
                         <div className="flex justify-around flex-wrap">
                             <div className="flex gap-6 items-center">
                                 <label htmlFor="sim" className="w-10">Sim</label>
-                                <input type="radio" value={1} name="sim"/>
+                                <input type="radio" value={publico} name="sim" onChange={(event) => {setPublico(event.target.value)}}/>
                             </div>
                             <div className="flex gap-6 items-center">
                                 <label htmlFor="nao" className="w-10">Não</label>
-                                <input type="radio" value={0} name="nao"/>
+                                <input type="radio" value={"false"} name="nao" onChange={(event) => {setPublico(event.target.value)}}/>
                             </div>
                         </div>
                     </div>
                     <div className="flex flex-col w-[40%] gap-4">
                         <label htmlFor="tema">Integrantes</label>
-                        <input type="text" name="tema" className="border" />
+                        <input type="text" name="tema" className="border" value={participantes} onChange={(event) => {setParticipante(event.target.value)}}/> 
                     </div>
                 </div>
                 <div className="flex gap-4 px-24 flex-col w-[90%]">
@@ -47,14 +59,15 @@ export default function novoProjeto(){
                     <label htmlFor="descr" className="px-2">Descrição</label>
                     <div className="flex flex-col w-auto gap-4 flex-wrap">
                         <div className="w-auto px-2">
-                            <textarea  className="border w-full p-2 h-28" name="descr"/>
+                            <textarea  className="border w-full p-2 h-28" name="descr" value={descricao}
+                            onChange={(event) => {setDescricao(event.target.value)}}/>
                         </div>
                     </div>
                 </div>
 
                 <div className="flex justify-around w-full">
                     <button className="bg-slate-500 p-2 rounded w-32 text-fontButton">Voltar</button>
-                    <button className="bg-buttonActivated p-2 w-32 rounded text-fontButton">Criar Projeto</button>
+                    <button className="bg-buttonActivated p-2 w-32 rounded text-fontButton" onClick={() => {CriarProjeto()}}>Criar Projeto</button>
                 </div>
             </div>
 
